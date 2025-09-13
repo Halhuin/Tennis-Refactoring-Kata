a157c0ee2c8fd11e28bd58349a071cc2b75f71b4
Halhuin <theo.naniche1@gmail.com>
2025-09-13 15:57:16 +0200

    Extracted a method to convert m_score into tennis score

--- javascript-jest/tennis1.js
+++ javascript-jest/tennis1.js
@@ -31,23 +31,23 @@ function getScore(m_score1, m_score2) {
                 score += "-";
                 tempScore = m_score2;
             }
-            switch (tempScore) {
-                case 0:
-                    score += "Love";
-                    break;
-                case 1:
-                    score += "Fifteen";
-                    break;
-                case 2:
-                    score += "Thirty";
-                    break;
-                case 3:
-                    score += "Forty";
-                    break;
-            }
+            score += tryConvertToTennisScore(tempScore);
         }
     }
     return score;
+}
+
+function tryConvertToTennisScore(m_score){
+    switch (m_score) {
+        case 0:
+            return "Love";
+        case 1:
+            return "Fifteen";
+        case 2:
+            return "Thirty";
+        case 3:
+            return "Forty";
+    }
 }
 
 module.exports = getScore;
